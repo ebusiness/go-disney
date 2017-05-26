f55469ec9653d1c2de18f71ca0c1b1b8139f6ff5
Xinguang Wang <wangxinguang@e-business.co.jp>
2017-05-26 19:51:05 +0900

    bash

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,67 +1,81 @@
-#!/bin/sh
+#!/bin/bash
 
-if ! command -v golint >/dev/null; then
+if [ ! -n "$BASH" ] ;then
+  # echo Please run this script $0 with bash;
+  if ! command -v bash >/dev/null; then
+    echo "install bash"
+    apk add --update bash
+  fi
+  bash ./scripts/setup.sh
+  exit 0;
+fi
 
+
+setup::install::git() {
   if ! command -v git >/dev/null; then
     echo "install git"
     # alpine
     apk add --update git
-
   fi
-
+}
+setup::install::golint() {
+  setup::install::git
   echo "install golint"
   go get -u github.com/golang/lint/golint
-fi
+}
 
-# command -v golint >/dev/null 2>&1 || { echo >&2 "I require golint but it's not installed.  Aborting."; exit 1; }
-
-
-# golint ./...
-echo "golint"
-golint config/*.go
-golint middleware/*.go
-golint utils/*.go
-golint v1/*.go
-golint v1/models/*.go
-golint v1/algorithms/*.go
-
-echo "go fmt"
-# go fmt ./...
-# go fmt *.go
-# go fmt config/*.go
-# go fmt middleware/*.go
-# go fmt utils/*.go
-# go fmt v1/*.go
-# go fmt v1/models/*.go
-
-gofmt -s -w -l  *.go
-gofmt -s -w -l  config/*.go
-gofmt -s -w -l  middleware/*.go
-gofmt -s -w -l  utils/*.go
-gofmt -s -w -l  v1/*.go
-gofmt -s -w -l  v1/models/*.go
-gofmt -s -w -l  v1/algorithms/*.go
-
-
-echo "check vendor"
-if [[ ! -d /go/src/github.com/ebusiness/go-disney/vendor ]]; then
-  echo "get dep"
-
+setup::install::dep() {
   if ! command -v dep >/dev/null; then
-
-    if ! command -v git >/dev/null; then
-      echo "install git"
-      # alpine
-      apk add --update git
-
-    fi
-
+    setup::install::git
     echo "install dep"
     go get -u github.com/golang/dep/...
   fi
-  # dep init
-  echo "ensure"
+}
 
-  dep ensure
-  # dep ensure -update
-fi
+setup::golint() {
+  if ! command -v golint >/dev/null; then
+    setup::install::golint
+  fi
+  echo "golint"
+  golint config/*.go
+  golint middleware/*.go
+  golint utils/*.go
+  golint v1/*.go
+  golint v1/models/*.go
+  golint v1/algorithms/*.go
+}
+
+setup::gofmt() {
+  echo "go fmt"
+  # go fmt ./...
+  # go fmt *.go
+  # go fmt config/*.go
+  # go fmt middleware/*.go
+  # go fmt utils/*.go
+  # go fmt v1/*.go
+  # go fmt v1/models/*.go
+
+  gofmt -s -w -l  *.go
+  gofmt -s -w -l  config/*.go
+  gofmt -s -w -l  middleware/*.go
+  gofmt -s -w -l  utils/*.go
+  gofmt -s -w -l  v1/*.go
+  gofmt -s -w -l  v1/models/*.go
+  gofmt -s -w -l  v1/algorithms/*.go
+}
+
+
+setup::vendor() {
+  echo "check vendor"
+  if [[ ! -d /go/src/github.com/ebusiness/go-disney/vendor ]]; then
+    setup::install::dep
+    # dep init
+    echo "ensure"
+    dep ensure
+  fi
+}
+
+setup::golint
+setup::gofmt
+setup::vendor
+exit 0;
